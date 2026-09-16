cdd080b06ca0c3aac8741b22280b9994358a438a
ledriczscorptec <ledriczn@scorptec.com.au>
2026-09-16 05:27:44 +0000

    proc

---
+++ procurement_purchase_no_grouping/models/__init__.py
@@ -0,0 +1,6 @@
+from . import product_category
+# from . import stock_rule
+from . import stock_rule_custom
+from . import purchase_order
+from . import res_config_settings
+from . import res_company
